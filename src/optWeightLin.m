2869c69018940b5c55c72cd3a637501ed68baa6a
joergbuescher <jrb@brain-biotech.de>
2013-06-14 12:01:56 +0200

    added comment in optWeightLin.m

--- src/optWeightLin.m
+++ src/optWeightLin.m
@@ -1,4 +1,9 @@
 function obj = optWeightLin(weight, highscoreVectors, lowscoreVectors)
+% this is the objective function for the linear optimization of the weighting of 
+% the individual scores. It maximizes the difference between the weighted sum of 
+% scores of the correctly assigned reactions and the incorrect matches in the 
+% training data set.
+%
     obj = 1/(abs(mean(highscoreVectors'*weight) - ...
                  mean(lowscoreVectors'*weight))+1) ;
 end
